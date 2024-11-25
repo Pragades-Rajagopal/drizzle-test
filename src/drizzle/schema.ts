c28b1ad4e01e762cc07362c147f2989f6f916fa1
Pragades Rajagopal <pragadesgoldsmith@outlook.com>
2024-11-25 22:20:36 +0530

    schema change, basic crud operations

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,6 +1,115 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  uniqueIndex,
+  integer,
+  pgEnum,
+  pgTable,
+  uuid,
+  varchar,
+  real,
+  time,
+  primaryKey,
+} from "drizzle-orm/pg-core";
 
-export const UserTable = pgTable("user", {
+export const UserRole = pgEnum("userRole", ["Basic", "Admin"]);
+
+export const UserTable = pgTable(
+  "user",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    age: integer("age").notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    role: UserRole("userRole").default("Basic").notNull(),
+  },
+  (table) => [uniqueIndex("emailIndex").on(table.email)]
+);
+
+export const UserPreferencesTable = pgTable("userPreferences", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  emailUpdates: boolean("emailUpdates").notNull().default(false),
+  userId: uuid("userId")
+    .references(() => UserTable.id)
+    .notNull(),
+});
+
+export const PostTable = pgTable("post", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: varchar("title", { length: 255 }).notNull(),
+  averageRating: real("averageRating").notNull().default(0),
+  createdAt: time("createdAt").notNull().defaultNow(),
+  updatedAt: time("updatedAt").notNull().defaultNow(),
+  authorId: uuid("authorId")
+    .references(() => UserTable.id)
+    .notNull(),
+});
+
+export const CategoryTable = pgTable("category", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 255 }).notNull(),
 });
+
+export const PostCategoryTable = pgTable(
+  "postCategory",
+  {
+    postId: uuid("postId")
+      .references(() => PostTable.id)
+      .notNull(),
+    categoryId: uuid("categoryId")
+      .references(() => CategoryTable.id)
+      .notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.postId, table.categoryId] })]
+);
+
+export const UserTableRelations = relations(UserTable, ({ one, many }) => {
+  return {
+    preferences: one(UserPreferencesTable),
+    posts: many(PostTable),
+  };
+});
+
+export const UserPreferencesTableRelations = relations(
+  UserPreferencesTable,
+  ({ one }) => {
+    return {
+      user: one(UserTable, {
+        fields: [UserPreferencesTable.userId],
+        references: [UserTable.id],
+      }),
+    };
+  }
+);
+
+export const PostTableRelations = relations(PostTable, ({ one, many }) => {
+  return {
+    author: one(UserTable, {
+      fields: [PostTable.authorId],
+      references: [UserTable.id],
+    }),
+    postCategories: many(PostCategoryTable),
+  };
+});
+
+export const CategoryTableRelations = relations(CategoryTable, ({ many }) => {
+  return {
+    postCategories: many(PostCategoryTable),
+  };
+});
+
+export const PostCategoryTableRelations = relations(
+  PostCategoryTable,
+  ({ one }) => {
+    return {
+      post: one(PostTable, {
+        fields: [PostCategoryTable.postId],
+        references: [PostTable.id],
+      }),
+      category: one(CategoryTable, {
+        fields: [PostCategoryTable.categoryId],
+        references: [CategoryTable.id],
+      }),
+    };
+  }
+);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,94 @@
 import "dotenv/config";
 import { db } from "./drizzle/db";
-import { UserTable } from "./drizzle/schema";
+import { UserPreferencesTable, UserTable } from "./drizzle/schema";
+import { asc, between, sql, eq, count, gte } from "drizzle-orm";
 
 (async () => {
-  await db.insert(UserTable).values({
-    name: "Max",
-  });
-  const user = await db.query.UserTable.findFirst();
-  console.log(user);
+  // await db.delete(UserTable);
+  // const userId = await db
+  //   .insert(UserTable)
+  //   .values([
+  //     {
+  //       name: "Max",
+  //       age: 30,
+  //       email: "max@app.com",
+  //     },
+  //     {
+  //       name: "Alex",
+  //       age: 35,
+  //       email: "alex@app.com",
+  //     },
+  //   ])
+  //   .returning({
+  //     id: UserTable.id,
+  //     userName: UserTable.name,
+  //   }).onConflictDoUpdate({
+  //     target: UserTable.email,
+  //     set: {name: "Updated name"}
+  //   });
+  // console.log(userId);
+
+  // await db.insert(UserPreferencesTable).values({
+  //   emailUpdates: true,
+  //   userId: "8acef599-9657-45ae-ab1a-29c4cde75966",
+  // });
+
+  // const users = await db.query.UserTable.findMany({
+  //   columns: { id: true, name: true },
+  //   // extras: {
+  //   //   lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName"),
+  //   // },
+  //   with: {
+  //     preferences: {
+  //       columns: {
+  //         emailUpdates: true,
+  //       },
+  //     },
+  //     posts: {
+  //       with: {
+  //         postCategories: true,
+  //       },
+  //     },
+  //   },
+  //   orderBy: asc(UserTable.name),
+  //   where: (table, functions) => functions.between(table.age, 20, 40),
+  // });
+  // console.log(users);
+
+  // const users = await db
+  //   .select({
+  //     id: UserTable.id,
+  //     name: UserTable.name,
+  //     age: UserTable.age,
+  //     // preference: { emailUpdated: UserPreferencesTable.emailUpdates },
+  //     emailUpdated: UserPreferencesTable.emailUpdates,
+  //   })
+  //   .from(UserTable)
+  //   .where(between(UserTable.age, 20, 40))
+  //   .fullJoin(
+  //     UserPreferencesTable,
+  //     eq(UserPreferencesTable.userId, UserTable.id)
+  //   );
+
+  // const users = await db
+  //   .select({
+  //     name: UserTable.name,
+  //     count: count(UserTable.name),
+  //   })
+  //   .from(UserTable)
+  //   .groupBy(UserTable.name)
+  //   .having((columns) => gte(columns.count, 1));
+
+  const users = await db.select().from(UserTable);
+  console.log(users);
+
+  const update = await db
+    .update(UserTable)
+    .set({
+      age: 40,
+    })
+    .where(eq(UserTable.id, "4fcc1211-f99b-44bb-b879-1ad03bcc54e0"));
+
+  const users1 = await db.select().from(UserTable);
+  console.log(users1);
 })();
